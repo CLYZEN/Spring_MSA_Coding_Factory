45910a9b82e5cd19935f2219aca5891947151a04
clyzen <jimuny01@gmail.com>
2024-01-09 10:28:25 +0900

    :sparkles: docker-compose.yml add :sparkles: Dockerfile add

--- License Service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ License Service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,8 +3,10 @@ package com.optimagrowth.license.controller;
 import com.optimagrowth.license.entity.License;
 import com.optimagrowth.license.service.LicenseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Locale;
 
@@ -22,6 +24,16 @@ public class LicenseController {
             @PathVariable("licenseId") String licenseId
     ) {
         License license = licenseService.getLicense(licenseId,organizationId);
+        license.add(
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                        .getLicense(organizationId,license.getLicenseId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                        .createLicense(organizationId,license,null)).withRel("createLicense"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                        .updateLicense(organizationId,license)).withRel("updateLicense"),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(LicenseController.class)
+                        .deleteLicense(organizationId,license.getLicenseId())).withRel("deleteLicense")
+        );
 
         return ResponseEntity.ok(license);
     }
--- License Service/src/main/java/com/optimagrowth/license/entity/License.java
+++ License Service/src/main/java/com/optimagrowth/license/entity/License.java
@@ -1,17 +1,21 @@
 package com.optimagrowth.license.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ToString
-public class License {
+@EqualsAndHashCode(callSuper = false)
+public class License extends RepresentationModel<License> {
     private int id;
     private String licenseId;
     private String description;
     private String organizationId;
     private String productName;
     private String licenseType;
+
 }
